d934d31c69ab4ed2c192330c0605435775779182
Vitor dos Santos Huçulak <vitor.huculak_ext@aldo.com.br>
2024-12-16 13:26:09 -0300

    altera o server.js

--- src/server.js
+++ src/server.js
@@ -19,5 +19,5 @@ server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
   console.log ("Boas vindas a API do Allbooks")
-  console.log("Acesse essa API na URL http://localhost:8000")
+  console.log("API disponivel atravez da URL http://localhost:8000")
 })
